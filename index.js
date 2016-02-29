1ab8a3c559b5c63938d0d1ffdfc9123ab8732dd2
Lucas Dohmen <lucas@dohmen.io>
2016-02-29 17:23:36 +0100

    Integrate Backend Service

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
 const app = express()
+const backend = require('./lib/backend')
 
 // Set up Mustache as the view engine
 app.engine('mustache', mustacheExpress())
@@ -8,14 +9,21 @@ app.set('views', './views')
 app.set('view engine', 'mustache')
 app.set('layout', 'layout')
 
-// Mouunt the assets
+// Mount the assets
 app.use('/assets', express.static('public'))
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'World' })
+  backend.household('4711', (err, data) => {
+    if (err) {
+      throw err
+    }
+    res.render('index', {
+      household: data,
+      title: 'World'
+    })
+  })
 })
 
 app.listen(8080, function () {
   console.log('Listening on port 8080!')
 })
-
