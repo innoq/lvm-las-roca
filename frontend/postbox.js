0c47c0dc78c0900c4de02994b8ba6b8bc7d62db7
Marc Jansing <marc.jansing@innoq.com>
2016-03-09 17:36:34 +0100

    basic postbox preview rendering

--- frontend/postbox.js
+++ frontend/postbox.js
@@ -8,7 +8,23 @@ $.ajax(postboxLink.attr('href'), {
   // success
   let messagesTable = $(data).find('table.messages-overview')
   let messageCount = messagesTable.find('tbody>tr').length
+
+  // set message count
   postboxLink.attr('data-bubble', messageCount)
+
+  // render preview
+  postboxLink.data({
+    'content': renderPreview(messagesTable),
+    'html': true,
+    'placement': 'bottom',
+    'title': renderPreviewTitle(postboxLink.attr('href')),
+    'toggle': 'popover'
+  }).on('click', (e) => {
+    e.preventDefault()
+  }).popover({
+    container: 'body',
+    trigger: 'click'
+  })
 }).fail(() => {
   // fail
   console.log('ERROR: postbox currently unavailable')
@@ -20,7 +36,21 @@ $.ajax(postboxLink.attr('href'), {
   }).on('click', (e) => {
     e.preventDefault()
   }).popover({
-    trigger: 'hover focus',
-    container: 'body'
+    container: 'body',
+    trigger: 'hover focus'
   })
 })
+
+function renderPreview (messagesTable) {
+  // TODO: modify preview markup
+  let result = $('<div>').append(messagesTable).clone()
+  return result.html()
+}
+
+function renderPreviewTitle (uri) {
+  let title = `<h3 class="collection-excerpt-title">
+                  Benachrichtigungen
+                  <a href="${uri}" class="collection-link">Alle anzeigen</a>
+              </h3>`
+  return $(title).html()
+}
