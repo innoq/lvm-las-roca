e6c8621b15afdba8a162d14b3be9d21bc6ae82a6
Lucas Dohmen <lucas@dohmen.io>
2016-03-18 11:41:21 +0100

    Adjust tests

--- tests/search_test.js
+++ tests/search_test.js
@@ -2,28 +2,28 @@ const test = require('tape')
 const helper = require('./test_helper')
 
 helper.setup((browser) => {
-  test('Search for customer Peter Produkt', (t) => {
+  test('Search for customer Peter Meyer', (t) => {
     t.plan(1)
 
     browser.visit('/').then(() => {
       browser.assert.success()
-      return browser.fill('Suche', 'Produkt')
+      return browser.fill('Suche', 'Meyer')
         .pressButton('Suchen')
     }).then(() => {
       browser.assert.success()
       browser.assert.url({
         pathname: '/result',
-        query: { query: 'Produkt' }
+        query: { query: 'Meyer' }
       })
-      browser.assert.text('h1', "Suchergebnisse für 'Produkt'")
+      browser.assert.text('h1', "Suchergebnisse für 'Meyer'")
       let link = browser.queryAll('.onebox-results a').find((el) => {
-        return browser.text('h3', el) === 'Herr Produkt, Peter'
+        return browser.text('h3', el) === 'Herr Peter Meyer'
       })
       return browser.click(link)
     }).then(() => {
       browser.assert.success()
       browser.assert.url({ pathname: '/partners/4711' })
-      browser.assert.text('.p-name', 'Herr Peter Produkt')
+      browser.assert.text('.p-name', 'Herr Peter Meyer')
       t.pass('Found correct partner page')
     })
   })
